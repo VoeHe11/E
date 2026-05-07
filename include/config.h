5d574521af531b1e2d7626de0609f3cc8c9f8122
Henri <henriundesther@googlemail.com>
2026-05-07 08:32:16 +0200

    Added initial structure

---
+++ include/config.h
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2026
+
+//
+// File:    config.h
+// Project: E
+//
+// Module:  TODO
+//
+// Created:  07.05.2026 – henri
+// Modified: 07.05.2026
+//
+//
+#ifndef E_CONFIG_H
+#define E_CONFIG_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif /* E_CONFIG_H */
